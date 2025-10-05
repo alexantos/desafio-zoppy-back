c5e07b61d6a4ee48308eac27389966219b9855d0
alexantos <xandreale09@gmail.com>
2025-10-05 12:30:22 -0300

    Adição do CORS config.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	const corsOptions: CorsOptions = {
+		origin: ['http://localhost:4200'],
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+		allowedHeaders: 'Content-Type, Accept, Authorization',
+	};
+	app.enableCors();
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
